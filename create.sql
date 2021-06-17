97c1a16af08befe727a9f341f2c8e816d5cec021
RivinSainudeen <47632884+RivinSainudeen@users.noreply.github.com>
2021-06-17 14:28:24 +0530

    Sixth attempt

--- create.sql
+++ create.sql
@@ -1,22 +1,24 @@
-CREATE TABLE publisher (
-	id INTEGER PRIMARY KEY,
-	name TEXT,
-	country TEXT
-	);
-	
+CREATE TABLE publisher(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    country TEXT NOT NULL
+);
+
 CREATE TABLE books(
-	id INTEGER PRIMARY KEY,
-	title TEXT,
-	publisher INTEGER REFERENCES publisher (id)
-	);
-	
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    publisher INTEGER NOT NULL,
+    FOREIGN KEY (publisher) REFERENCES publisher (id) 
+);
+
 CREATE TABLE subjects(
-	id INTEGER PRIMARY KEY,
-	name TEXT 
-	);
-	
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
 CREATE TABLE books_subjects(
-	book INTEGER REFERENCES books(id),
-	subject INTEGER REFERENCES subjects (id)
-	);
-	
+    book INTEGER NOT NULL,
+    subject INTEGER NOT NULL,
+    FOREIGN KEY (book) REFERENCES books (id),
+    FOREIGN KEY (subject) REFERENCES subjects (id)
+);
