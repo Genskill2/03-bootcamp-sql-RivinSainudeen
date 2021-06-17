4b697d11f6a63940c21caf3f29760f22e26ae6aa
RivinSainudeen <47632884+RivinSainudeen@users.noreply.github.com>
2021-06-17 14:24:44 +0530

    Second attempt

--- delete1.sql
+++ delete1.sql
@@ -1,8 +1,3 @@
-delete from books_subjects
-where subject in (
-	select ID from subjects
-	where name = "History"
-	);
+delete from books_subjects where subject in (select ID from subjects where name = "History");
 	
-delete from subjects 
-where name = "History";
+delete from subjects where name = "History";
