9f28c3a203f5c307847a197908bc40aa714e5976
RivinSainudeen <47632884+RivinSainudeen@users.noreply.github.com>
2021-06-19 16:00:00 +0530

    Updated

--- insert.sql
+++ insert.sql
@@ -1,59 +1,11 @@
-insert into publisher (name, country) 
-values
-    ("PHI", "India"),
-    ("Harper", "USA"),
-    ("GCP", "USA"),
-    ("Avery", "USA"),
-    ("Del Rey", "UK"),
-    ("Vintage", "UK");
+INSERT INTO publisher(id,name,country)
+VALUES(1,'PHI','India'),(2,'Harper','USA'),(3,'GCP','USA'),(4,'Avery','USA'),(5,'Del Rey','UK'),(6,'Vintage','UK');
 
+INSERT INTO books(id,title,publisher)
+VALUES(1,'The C Programming Language',1),(2,'The Go Programming Language',1),(3,'The UNIX Programming Environment',1),(4,'Cryptonomicon',2),(5,'Deep Work',3),(6,'Atomic Habits',4),(7,'The City and The City',5),(8,'The Great War for Civilisation',6);
 
-insert into books (title, publisher)
-values
-    ("The C Programming Language", 1),
-    ("The Go Programming Language", 1),
-    ("The UNIX Programming Environment", 1),
-    ("Cryptonomicon", 2),
-    ("Deep Work", 3),
-    ("Atomic Habits", 4),
-    ("The City and The City", 5),
-    ("The Great War for Civilisation", 6);
-
-
-
-insert into subjects (name)
-values
-    ("C"),
-    ("UNIX"),
-    ("Technology"),
-    ("Go"),
-    ("Science Fiction"),
-    ("Productivity"),
-    ("Psychology"),
-    ("Politics"),
-    ("History");
-
-
-
-insert into books_subjects (book, subject)
-values
-    (1, 1),
-    (1, 2),
-    (1, 3),
-    (2, 4),
-    (2, 3),
-    (3, 2),
-    (3, 3),
-    (4, 3),
-    (4, 5),
-    (5, 3),
-    (5, 6),
-    (6, 6),
-    (6, 7),
-    (7, 5),
-    (7, 8),
-    (8, 8),
-    (8, 9);
-	
-
-	
+INSERT INTO subjects(id,name)
+VALUES(1,'C'),(2,'UNIX'),(3,'Technology'),(4,'Go'),(5,'Science Fiction'),(6,'Productivity'),(7,'Psychology'),(8,'Politics'),(9,'History');
+   
+INSERT INTO books_subjects(book,subject)
+VALUES(1,1),(1,2),(1,3),(2,4),(2,3),(3,2),(3,3),(4,3),(4,5),(5,3),(5,6),(6,6),(6,7),(7,5),(7,8),(8,8),(8,9);
